690310550acc96964a58fa8895636a3ca5b28fdf
Tyler Moore <tyler.moore@rajones.com>
2022-03-25 18:32:44 -0400

    Updated header.php to proper sequence

--- pages/header.php
+++ pages/header.php
@@ -59,18 +59,14 @@ $group_names = find_all_groups();
 
             <?php if ($session->isUserLoggedIn()):?> 
               <button type="button" class="astext" id ="edit" title="Edit Password" data-toggle="modal" data-target="#edit_password_modal<?=$user['id'];?>" >Edit Password</button>
-
               <button type="button" class="astext" id ="edit" title="Edit Account" data-toggle="modal" data-target="#edit_user_modal<?=$user['id'];?>" >Edit Account</button>
-
-              <a href="/users/logout.php" target="_self">Logout</a>
-            
-            <?php else:?>
-              <a href="/users/index.php" target="_self">Login</a>      
-            <?php endif;?>
-            
-            <?php if ($_SESSION['userLevel']==1):?> 
+              <?php if ($_SESSION['userLevel']==1):?> 
               <a href="/users/add_user.php" target="_self">Add User</a>
               <a href="/users/users.php" target="_self">Users</a>
+            <?php endif;?>
+            <a href="/users/logout.php" target="_self">Logout</a>
+            <?php else:?>
+              <a href="/users/index.php" target="_self">Login</a>      
             <?php endif;?>
             
           </div>
