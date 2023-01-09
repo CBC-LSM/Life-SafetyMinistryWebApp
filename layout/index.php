690fcdc792e1fab496e72fe95b3f0d655756bc1b
Tyler Moore <tyler.moore@rajones.com>
2023-01-09 09:05:51 -0500

    testing picture mapping

---
+++ layout/index.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<body>
+<img src="../images/buildingLayout.png" usemap="#green" height="800" width="1200" border="3">
+<map name="green">
+<area shape="polygon" coords="19,44,45,11,87,37,82,76,49,98" href="http://www.trees.com/save.html" data-toggle="tooltip" title="test">
+<area shape="rect" coords="128,132,241,179" href="http://www.trees.com/furniture.html">
+<area shape="circle" coords="500,500,200" href="http://www.trees.com/plantations.html" data-toggle="tooltip" title="test">
+</map>
+</body>
