52a2863b7d9b0adb593136565b7c194edf7afad0
root <root@localhost.localdomain>
2023-10-31 22:58:04 +0000

    First Commmit of Checkout Display

---
+++ pcotest/checkoutDisplay.php
@@ -0,0 +1,61 @@
+<?php
+	include 'config.php';
+	date_default_timezone_set("America/New_York");
+	$checkoutData = curl_init();
+	curl_setopt($checkoutData, CURLOPT_URL, $APIURL . "&where[created_at]=" . date("Y-m-d") . "&per_page=15&order=-checked_out_at&include=locations");
+	curl_setopt($checkoutData, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($checkoutData, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	curl_setopt($checkoutData, CURLOPT_USERPWD, "$APIUser:$APIPass");
+	
+	$checkoutResponse = curl_exec($checkoutData);
+	
+	$jsonCheckout = json_decode($checkoutResponse, true);
+	curl_close($checkoutData);
+	$locationsArray = explode(",",$_GET['locations']);
+
+?>
+
+<html>
+<head>
+ <style>
+ body{
+   text-align: right;
+ }
+
+ h1{
+   font-size: 60px;
+   color: white;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: inline-block;
+  margin: 2px;
+
+ }
+
+  .child {
+    text-align: right;
+    font-size: 50px;
+    padding: 5px;
+    padding-left: 20px;
+    padding-right: 20px;
+    color: white;
+    background-color: rgba(0, 0, 0, 0.5);
+    //float: right;
+    display: inline-block;
+  }
+ </style>
+ <meta http-equiv="refresh" content="30">
+</head>
+<body>
+  <h1>Pickup:</h1><br>
+<?php
+foreach($jsonCheckout['data'] as $checkoutEvent){
+	if(!empty($checkoutEvent['attributes']['checked_out_at'])){
+		if(in_array($checkoutEvent['relationships']['locations']['data'][0]['id'], $locationsArray)){
+			echo "<div class='child'>" . $checkoutEvent['attributes']['first_name'] . " " . $checkoutEvent['attributes']['last_name'] . "</div><br>";
+		}
+	}	
+}
+?>
+
+</body>
+</html>
---
+++ pcotest/config.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Configuration id's from PCO
+Sunday Children's Ministry - EventID: 27232922
+	Baby Nursery - LocationID: 1451137
+	Toddler Nursery - LocationID: 1451138
+	Children's Ministry Admin - LocationID: 1451155
+	Sunday School Hour - LocationID: 1451144
+		2s and 3s - LocationID: 1451152
+		4s and 5s - LocationID: 1451154
+		K and 1st Grade - LocationID: 1451149
+		2nd and 3rd Grade - LocationID: 1451146
+		4th and 5th Grade - LocationID: 1451147
+	Worship Hour - LocationID: 1451145
+		Disciple Kids Worship Jr. - LocationID: 1451139
+		Disciple Kids Worship - LocationID: 1451140
+AWANA - EventID: 27121386
+	AWANA Staff - LocationID: 248040
+	AWANA Security Checkin - LocationID: 261759
+	Baby Nursery - LocationID: 261760
+	Toddler Nursery - LocationID: 261761
+	Puggles - LocationID: 233759
+	Cubbies - LocationID: 233761
+	Sparks - LocationID: 233760
+	TNT - LocatinID: 233762
+*/
+
+// API Credentials
+$APIUser = '596c41b067090eca4448f693976977ebcffeeee64d1f1ba92d9b921432f3575a';
+$APIPass = '2da8fcb705df3b927f1f9c17d40fc44478a283924e6e90a536576f284437571d';
+$APIURL = "https://api.planningcenteronline.com/check-ins/v2/check_ins?order=-checked_out_at";
+
+?>
