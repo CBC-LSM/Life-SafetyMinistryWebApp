62032626e9024030f39f5ca288d75410b52faa28
root <root@localhost.localdomain>
2024-01-08 00:27:31 +0000

    added style sheet to clean up code a little

--- pcoCheckout/dkjstudio.php
+++ pcoCheckout/dkjstudio.php
@@ -17,39 +17,8 @@
 
 <html>
 <head>
- <style>
- body{
-   text-align: left;
-   /*background-image: url("dkbackground.jpg");*/
-   background-image: linear-gradient(#e79323, #c83d96);
-   background-size: 100vw 100vh;
- }
+  <link rel="stylesheet" href="checkoutstyle.css" />
 
- h1{
-   font-size: 80px;
-  margin-left: auto;
-  margin-right: auto;
-  text-align: center;
-
- }
-
-  .child {
-    padding: 0px;
-    margin-right: 40px;
-    font-size: 50px;
-    text-wrap: nowrap;
-    white-space: nowrap;
-    overflow-x: hidden;
-    float: left;
-    width: 30vw;
-  }
-
-  ul {
-    display: table;
-    margin: 0 auto;
-    text-align: left;
-  }
- </style>
  <?php
 if(isset($_GET['locations'])){
 ?>
@@ -63,40 +32,40 @@ if(isset($_GET['locations'])){
 <?php
 if(!isset($_GET['locations'])){
 ?>
-<p>Locations Parameter not set.  Please add ?locations= followed by the location id's seperated by comma's to the end of the url.</0>
+<!-- <p>Locations Parameter not set.  Please add ?locations= followed by the location id's seperated by comma's to the end of the url.</0> -->
   <div class="locationsList">
   <ul>
     <li>Sunday Children's Ministry - EventID: 27354867</li>
 	  <ul>
-      <li>Baby Nursery - LocationID: 1452290</li>
-      <li>Toddler Nursery - LocationID: 1452291</li>
-      <li>Children's Ministry Admin - LocationID: 1452301</li>
-      <li>Sunday School Hour - LocationID: 1452292</li>
+      <li><a href="?locations=%1452290">Baby Nursery - LocationID: 1452290</a></li>
+      <li><a href="?locations=%1452291">Toddler Nursery - LocationID: 1452291</a></li>
+      <li><a href="?locations=%1452301">Children's Ministry Admin - LocationID: 1452301</a></li>
+      <li><a href="?locations=%1452292">Sunday School Hour - LocationID: 1452292</a></li>
       <ul>
-        <li>2s and 3s - LocationID: 1452296</li>
-        <li>4s and 5s - LocationID: 1452297</li>
-        <li>K and 1st Grade - LocationID: 1452295</li>
-        <li>2nd and 3rd Grade - LocationID: 1452293</li>
-        <li>4th and 5th Grade - LocationID: 1452294</li>
+        <li><a href="?locations=%1452296">2s and 3s - LocationID: 1452296</a></li>
+        <li><a href="?locations=%1452297">4s and 5s - LocationID: 1452297</a></li>
+        <li><a href="?locations=%1452295">K and 1st Grade - LocationID: 1452295</a></li>
+        <li><a href="?locations=%1452293">2nd and 3rd Grade - LocationID: 1452293</a></li>
+        <li><a href="?locations=%1452294">4th and 5th Grade - LocationID: 1452294</a></li>
       </ul>
-      <li>Worship Hour - LocationID: 1452298</li>
+      <li><a href="?locations=%1452298">Worship Hour - LocationID: 1452298</a></li>
         <ul>
-        <li>Disciple Kids Worship Jr. - LocationID: 1452299</li>
-        <li>Disciple Kids Worship - LocationID: 1452300</li>
+        <li><a href="?locations=%1452299">Disciple Kids Worship Jr. - LocationID: 1452299</a></li>
+        <li><a href="?locations=%1452300">Disciple Kids Worship - LocationID: 1452300</a></li>
         </ul>
   </ul>
   </ul>
   <ul>
     <li>AWANA - EventID: 27121386</li>
     <ul>
-      <li>AWANA Staff - LocationID: 248040</li>
-      <li>AWANA Security Checkin - LocationID: 261759</li>
-      <li>Baby Nursery - LocationID: 261760</li>
-      <li>Toddler Nursery - LocationID: 261761</li>
-      <li>Puggles - LocationID: 233759</li>
-      <li>Cubbies - LocationID: 233761</li>
-      <li>Sparks - LocationID: 233760</li>
-      <li>TNT - LocationID: 233762</li>
+      <li><a href="?locations=%248040">AWANA Staff - LocationID: 248040</a></li>
+      <li><a href="?locations=%261759">AWANA Security Checkin - LocationID: 261759</a></li>
+      <li><a href="?locations=%261760">Baby Nursery - LocationID: 261760</a></li>
+      <li><a href="?locations=%261761">Toddler Nursery - LocationID: 261761</a></li>
+      <li><a href="?locations=%233759">Puggles - LocationID: 233759</a></li>
+      <li><a href="?locations=%233761">Cubbies - LocationID: 233761</a></li>
+      <li><a href="?locations=%233760">Sparks - LocationID: 233760</a></li>
+      <li><a href="?locations=%233762">TNT - LocationID: 233762</a></li>
     </ul>
   </ul>
   </div>
